d270a207a6981144bc5036498fdb7124b72b8688
antarius73 <gjonik@gmail.com>
2016-03-14 15:54:22 +0100

    controle des col de la grille dans le controlleur

--- app/scripts/controllers/getmultiple.js
+++ app/scripts/controllers/getmultiple.js
@@ -10,10 +10,29 @@
 
 
 angular.module('anguarRestApp')
-  .controller('GetmultipleCtrl', function ($scope, $http) {
-    $http.get('http://jsonplaceholder.typicode.com/photos').
-    success(function(data){
-      $scope.images =data.slice(0,500);
+  .controller('GetmultipleCtrl', function ($resource, $scope, $http, DTOptionsBuilder, DTColumnDefBuilder) {
+
+    $scope.images=[];
+
+    // methode direct mais pour le mode generation auto de la dtt
+    //$scope.dtOptions = DTOptionsBuilder.fromSource('http://jsonplaceholder.typicode.com/photos').withDataProp('images');
+    $scope.dtOptions = DTOptionsBuilder.newOptions();
+
+    $scope.dtColumnDefs = [
+      DTColumnDefBuilder.newColumnDef(0),
+      DTColumnDefBuilder.newColumnDef(1),
+      DTColumnDefBuilder.newColumnDef(2).notSortable(),
+      DTColumnDefBuilder.newColumnDef(3).notVisible()
+   ];
+    // fixe la langue de la grille à la volé
+    $scope.dtOptions.withLanguageSource('//cdn.datatables.net/plug-ins/1.10.11/i18n/English.json');
+    $resource('http://jsonplaceholder.typicode.com/photos').query().$promise.then(function(images) {
+      $scope.images = images.slice(0,200);
     });
+    // aces a une source mode normal pour mémoire
+    //$http.get('http://jsonplaceholder.typicode.com/photos').
+    //success(function(data){
+    //  $scope.images =data.slice(0,500);
+    //});
 
   });
