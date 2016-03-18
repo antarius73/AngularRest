43d320caaafcb2a7f41a8b0c32cf1ec68c6181d8
antarius73 <gjonik@gmail.com>
2016-03-18 16:24:46 +0100

    finalisation fiche detail personne

--- app/scripts/controllers/persons.js
+++ app/scripts/controllers/persons.js
@@ -10,23 +10,24 @@
 angular.module('angularRestApp')
   .controller('PersonsController', function ($resource, $scope, DTOptionsBuilder, DTColumnDefBuilder, $rootScope, Persons) {
 
-    $scope.persons = [];
+     $scope.persons = [];
 
-    $scope.dtOptions = DTOptionsBuilder.newOptions();
+     $scope.dtOptions = DTOptionsBuilder.newOptions();
 
-    $scope.dtColumnDefs = [
-      DTColumnDefBuilder.newColumnDef(0),
-      DTColumnDefBuilder.newColumnDef(1),
-      DTColumnDefBuilder.newColumnDef(2),
-      DTColumnDefBuilder.newColumnDef(3),
-      DTColumnDefBuilder.newColumnDef(4)
-    ];
+     $scope.dtColumnDefs = [
+       DTColumnDefBuilder.newColumnDef(0),
+       DTColumnDefBuilder.newColumnDef(1),
+       DTColumnDefBuilder.newColumnDef(2),
+       DTColumnDefBuilder.newColumnDef(3),
+       DTColumnDefBuilder.newColumnDef(4)
+     ];
+
+     Persons.query().$promise.then(function (persons) {
+       $scope.persons = persons.slice(0, 200);
+     });
+
     // fixe la langue de la grille à la volé
     $scope.dtOptions.withLanguageSource('resources/datatable-'+$rootScope.Userlanguage+'.json');
-
-    Persons.query().$promise.then(function (persons) {
-      $scope.persons = persons.slice(0, 200);
-    });
 
 
   });
