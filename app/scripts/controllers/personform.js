500ee75781af9e8ba25817a633296b5d254b2fe7
antarius73 <gjonik@gmail.com>
2016-03-22 17:10:36 +0100

    formulaire de modification fonctionnel

--- app/scripts/controllers/personform.js
+++ app/scripts/controllers/personform.js
@@ -8,23 +8,27 @@
  * Controller of the angularRestApp
  */
 angular.module('angularRestApp')
-  .controller('PersonformCtrl', function ($scope, Person, $routeParams, $filter) {
+  .controller('PersonformCtrl', function ($scope, Person, $routeParams, $filter, $location) {
 
     $scope.format = "MM/dd/yyyy";
 
     Person.get({id: $routeParams.id}).$promise.then(function (person) {
       $scope.person = person;
-
-      $scope.modifiedDateUI = new Date( $filter('date')($scope.person.ModifiedDateString,"MM/dd/yyyy")) ;
-
+      // utilisation d'une variable tmp pour la gestion visuel de la date
+      $scope.person.modifiedDateUI = new Date( $filter('date')($scope.person.ModifiedDateString,"MM/dd/yyyy")) ;
     });
 
     $scope.openDatePicker = function(){
       $scope.isDatePickerOpen =true;
     };
 
+    $scope.update = function(user) {
+      // reconversion de la date dans le format REST
+      user.ModifiedDateString = new Date( $filter('date')($scope.person.modifiedDateUI,"MM/dd/yyyy")) ;
+      user.$update({id: user.Id}).then(function () {
+        console.log("retour");
+        $location.path('/persons');
+      });
 
-
-
-
+    };
   });
--- app/scripts/services/personservice.js
+++ app/scripts/services/personservice.js
@@ -2,29 +2,42 @@
 
 angular.module('angularRestApp')
 
-  .factory('Person', ['$resource',
-    function ($resource) {
-      return $resource('http://svr-grind.tesfri.intra:8080/Persons/:id/', {id: '@id'});
-      //,
-      //{
-      //  getDetail: {
-      //    method: 'GET',
-      //    params: {id: '@id'},
-      //    isArray: false
-      //  }
-      //}
-    }
 
+  .factory('Person', ['$resource', '$cacheFactory',
+
+    function ($resource, $cacheFactory) {
+      // supprimer le cache de la liste des personne si une personne est modifiée
+      var personsCache = $cacheFactory.get('persons');
+
+      var interceptor = {
+        response: function (response) {
+          if(typeof personsCache != 'undefined' )
+          personsCache.removeAll();
+
+          return response;
+        }
+      };
+
+      return $resource('http://svr-grind.tesfri.intra:8080/Persons/:id/', {id: '@id'},
+
+        {
+          'update': {method: 'PUT', interceptor: interceptor}
+        }
+      );
+    }
   ])
 
 
-  .factory('Persons', ['$resource','$cacheFactory',
-    function ($resource,$cacheFactory) {
-      var personsCache = $cacheFactory('persons')
+  .factory('Persons', ['$resource', '$cacheFactory',
+    function ($resource, $cacheFactory) {
+      var personsCache = $cacheFactory('persons');
+
+
       return $resource('http://svr-grind.tesfri.intra:8080/Persons/',
         {},
         {
-          'query': { method:'GET', cache: personsCache, isArray:true }
+          'query': {method: 'GET', cache: personsCache, isArray: true}
+
 
         }
       );
