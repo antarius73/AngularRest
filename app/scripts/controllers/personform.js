1d2cd267dfc7699870135390b9f300a521439e77
antarius73 <gjonik@gmail.com>
2016-03-25 15:22:14 +0100

    validation coté client upadte complete avec directive unique et traduction

--- app/scripts/controllers/personform.js
+++ app/scripts/controllers/personform.js
@@ -12,23 +12,45 @@ angular.module('angularRestApp')
 
     $scope.format = "MM/dd/yyyy";
 
-    Person.get({id: $routeParams.id}).$promise.then(function (person) {
-      $scope.person = person;
-      // utilisation d'une variable tmp pour la gestion visuel de la date
-      $scope.person.modifiedDateUI = new Date( $filter('date')($scope.person.ModifiedDateString,"MM/dd/yyyy")) ;
-    });
+    $scope.getPerson = function() {
+      Person.get({id: $routeParams.id}).$promise.then(function (person) {
+        $scope.person = person;
+        // utilisation d'une variable tmp pour la gestion visuel de la date
+        $scope.person.modifiedDateUI = new Date($filter('date')($scope.person.ModifiedDateString, "MM/dd/yyyy"));
+        $scope.person.originalFirstName = $scope.person.FirstName;
+      });
+    }
+
+    $scope.getPerson();
 
     $scope.openDatePicker = function(){
       $scope.isDatePickerOpen =true;
     };
 
+
+    $scope.resetUnique = function()
+    {
+      $scope.personForm.firstname.$setValidity("unique", true);
+    }
+
+    $scope.reset = function() {
+      $scope.getPerson();
+      $scope.personForm.$setPristine();
+    }
+
     $scope.update = function(user) {
+
+      $scope.showErrorsCheckValidity = true;
+
+
       // reconversion de la date dans le format REST
       user.ModifiedDateString = new Date( $filter('date')($scope.person.modifiedDateUI,"MM/dd/yyyy")) ;
       user.$update({id: user.Id}).then(function () {
         console.log("retour");
         $location.path('/persons');
       });
+
+
 
     };
   });
